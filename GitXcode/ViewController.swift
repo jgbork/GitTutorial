6c9f0158a24fd79c57b6f80eb0f0da310279ccb9
jgbden <krob56@comcast.net>
2017-05-15 11:30:23 -0600

    Added MapViewController

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
+        let mapViewController = ViewController()
+        print(mapViewController)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
